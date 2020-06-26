5733950d1376096d088e3b4759f2c95ef4e648ab
sandy-tartari <62676992+sandy-tartari@users.noreply.github.com>
2020-06-26 16:22:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InteiroMaior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int num;
+           Console.WriteLine("Digite um número maior que 0 (e tecle ENTER)");
+            num = int.Parse(Console.ReadLine());
+           Console.WriteLine("A sequência é:");
+            for(int i = 0; i <= num; i++)
+           {
+                Console.WriteLine(i);
+           }
+        }
+    }
+}
